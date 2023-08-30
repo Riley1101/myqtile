4096e33d2ef80bee36f7ec117980807123623be1
Riley1101 <arkardev00@gmail.com>
2023-08-30 10:13:16 -0400

    UPdate on funny status comment

--- config.py
+++ config.py
@@ -56,6 +56,7 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
@@ -146,6 +147,7 @@ screens = [
                 ),
                 widget.Wallpaper(
                     directory='/home/arkar/Pictures/wallpapers', random_selection=True, fmt=''),
+                widget.Spacer(length=5),
                 widget.Notify(),
                 widget.CPUGraph(type='line', line_width=1, border_width=0,
                                 fill_color=theme_current_line, graph_color=theme_purple, samples=50),
